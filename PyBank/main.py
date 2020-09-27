f24722c2cd952b8252c45ced75e8b2b1775bee1d
Kyle O'Malley <kyleomalley@Kyles-MBP.lan>
2020-09-27 14:27:40 -0400

    Pybank commit 1, total months

---
+++ PyBank/main.py
@@ -0,0 +1,25 @@
+# Attach modules
+import os
+import csv
+
+months = []
+profit_losses = []
+
+print("Financial Analysis")
+print("____________________________")
+
+# Set path for file analysis
+budget_csv = os.path.join("..", "Resources", "budget_data.csv")
+
+# Access the CSV file
+with open(budget_csv) as csvfile:
+    budget_reader = csv.reader(csvfile, delimiter=",")
+    #budget_header = next(csvfile)
+    #for row in budget_reader:
+
+        #find number of months
+    total_months = len(list(budget_reader))
+    print(f"Total Months: {total_months}")
+
+    
+        

1ee757f0a2b3c476984a4556cfea12f4857e2f46
Kyle O'Malley <kyleomalley@Kyles-MBP.lan>
2020-09-27 22:09:10 -0400

    added net profits

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,8 +4,7 @@ import csv
 
 month_count = 0
 months = []
-profit_losses = []
-total_months = []
+profit_losses = 0
 
 print("Financial Analysis")
 print("____________________________")
@@ -17,10 +16,16 @@ budget_csv = os.path.join("Resources", "budget_data.csv")
 with open(budget_csv) as csvfile:
     budget_reader = csv.reader(csvfile, delimiter=",")
     budget_header = next(budget_reader)
- 
+    # loop through rows of budget_reader
     for row in budget_reader:
+        #find total months
         month_count += 1
         months.append(row[0])
-        
+        # find net amount profits over period
+        monthly_profit_losses = int(row[1])
+        profit_losses += monthly_profit_losses
+
+
 
     print(f"Total Months: {month_count}")
+    print(f"Total Net Earning: {profit_losses}")

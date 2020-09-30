c58aa9033249c1106cf4446161bfc5e72b00a76e
Kyle O'Malley <kyleomalley@Kyles-MBP-51.lan>
2020-09-29 23:26:11 -0400

    found voter percentage, votes, and winner

--- PyPoll/mainpypoll.py
+++ PyPoll/mainpypoll.py
@@ -4,7 +4,10 @@ import csv
 total_votes = 0
 candidate_list = []
 votecount = {}
+vote_percent = {}
 candidate = ""
+winner_votes = 0
+winner = ""
 
 # Set path for file
 election_data_csv = os.path.join("Resources", "election_data.csv")
@@ -23,12 +26,21 @@ with open(election_data_csv) as csvfile:
         else:
             votecount[candidate] = 1
         
+    # Find voter percentage and winner
+    for candidate, votes in votecount.items():
+        vote_percent[candidate] = "{:.1%}".format(votes / total_votes)
+        if votes > winner_votes:
+            winner_votes = votes
+            winner = candidate
 
+print("Election Results")
+print("------------------------")
+print(f"Total Votes: {total_votes}")
+print("------------------------")
 
+for candidate, votes in votecount.items():
+    print(f"{candidate}: {vote_percent[candidate]} + {votes}")
 
-
-
-    print("Election Results")
-    print("------------------------")
-    print(f"Total Votes: {total_votes}")
-    print("------------------------")
+print("------------------------")
+print(f"Winner: {winner}")
+print("------------------------") 

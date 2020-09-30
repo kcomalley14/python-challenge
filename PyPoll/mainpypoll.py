0975504a8e17162981911b136dcf3abc8beaeef5
Kyle O'Malley <kyleomalley@Kyles-MBP-51.lan>
2020-09-29 21:06:23 -0400

    start of PyPoll commit, total up all votes

---
+++ PyPoll/mainpypoll.py
@@ -0,0 +1,34 @@
+import os
+import csv
+
+total_votes = 0
+candidate_list = []
+votecount = {}
+candidate = ""
+
+# Set path for file
+election_data_csv = os.path.join("Resources", "election_data.csv")
+
+# access csv file
+with open(election_data_csv) as csvfile:
+    election_reader = csv.reader(csvfile, delimiter= ",")
+    election_header = next(election_reader)
+
+    # Total up votes
+    for row in election_reader:
+        total_votes += 1
+        candidate = row[2]
+        if candidate in votecount:
+            votecount[candidate] += 1
+        else:
+            votecount[candidate] = 1
+        
+
+
+
+
+
+    print("Election Results")
+    print("------------------------")
+    print(f"Total Votes: {total_votes}")
+    print("------------------------")
